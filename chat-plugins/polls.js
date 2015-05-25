eb59ac909a724c6adb947e9c3d3001eed112b5c1
AustinXII <pingas311@hotmail.ca>
2015-05-25 11:12:20 -0600

    Update polls.js

--- chat-plugins/polls.js
+++ chat-plugins/polls.js
@@ -2,12 +2,20 @@ exports.commands = {
  
  
     poll: function (target, room, user) {
-        if (!this.can('broadcast')) return;
-        if (Poll[room.id].question) return this.sendReply('There is currently a poll going on already.');
-        if (!this.canTalk()) return;
+        if (!this.can('broadcast')) {
+            return;
+        }
+        if (Poll[room.id].question) {
+            return this.sendReply('There is currently a poll going on already.');
+        }
+        if (!this.canTalk()) {
+            return;
+        }
 
         var options = Poll.splint(target);
-        if (options.length < 3) return this.parse('/help poll');
+        if (options.length < 3) {
+            return this.parse('/help poll');
+        }
 
         var question = options.shift();
 
@@ -20,20 +28,26 @@ exports.commands = {
         var start = 0;
         while (start < Poll[room.id].optionList.length) {
             pollOptions += '<button name="send" value="/vote ' + Poll[room.id].optionList[start] + '">' + Poll[room.id].optionList[start] + '</button>&nbsp;';
-            start++;
+            start += 1;
         }
         Poll[room.id].display = '<h2>' + Poll[room.id].question + '&nbsp;&nbsp;<font size="1" color="#AAAAAA">/vote OPTION</font><br><font size="1" color="#AAAAAA">Poll started by <em>' + user.name + '</em></font><br><hr>&nbsp;&nbsp;&nbsp;&nbsp;' + pollOptions;
         room.add('|raw|<div class="infobox">' + Poll[room.id].display + '</div>');
     },
 
     tierpoll: function (target, room, user) {
-        if (!this.can('broadcast')) return;
+        if (!this.can('broadcast')) {
+            return;
+        }
         this.parse('/poll Tournament tier?, ' + Object.keys(Tools.data.Formats).filter(function (f) { return Tools.data.Formats[f].effectType === 'Format'; }).join(", "));
     },
 
     endpoll: function (target, room, user) {
-        if (!this.can('broadcast')) return;
-        if (!Poll[room.id].question) return this.sendReply('There is no poll to end in this room.');
+        if (!this.can('broadcast')) {
+            return;
+        }
+        if (!Poll[room.id].question) {
+            return this.sendReply('There is no poll to end in this room.');
+        }
 
         var votes = Object.keys(Poll[room.id].options).length;
 
@@ -49,7 +63,7 @@ exports.commands = {
         }
 
         for (var i in Poll[room.id].options) {
-            options[Poll[room.id].options[i]]++;
+            options[Poll[room.id].options[i]] += 1;
         }
 
         var data = [];
@@ -57,14 +71,13 @@ exports.commands = {
             data.push([i, options[i]]);
         }
         data.sort(function (a, b) {
-            return a[1] - b[1]
-        );
-        }
+            return a[1] - b[1];
+        });
 
         var results = '';
         var len = data.length;
         var topOption = data[len - 1][0];
-        while (len--) {
+        while (len -= 1) {
             if (data[len][1] > 0) {
                 results += '&bull; ' + data[len][0] + ' - ' + Math.floor(data[len][1] / votes * 100) + '% (' + data[len][1] + ')<br>';
             }
@@ -75,10 +88,18 @@ exports.commands = {
     },
  
  vote: function (target, room, user) {
-        if (!Poll[room.id].question) return this.sendReply('There is no poll currently going on in this room.');
-        if (!this.canTalk()) return;
-        if (!target) return this.parse('/help vote');
-        if (Poll[room.id].optionList.indexOf(target.toLowerCase()) === -1) return this.sendReply('\'' + target + '\' is not an option for the current poll.');
+        if (!Poll[room.id].question) {
+            return this.sendReply('There is no poll currently going on in this room.');
+        }
+        if (!this.canTalk()) {
+            return;
+        }
+        if (!target) {
+            return this.parse('/help vote');
+        }
+        if (Poll[room.id].optionList.indexOf(target.toLowerCase()) === -1) {
+            return this.sendReply('\'' + target + '\' is not an option for the current poll.');
+        }
 
         var ips = JSON.stringify(user.ips);
         Poll[room.id].options[ips] = target.toLowerCase();
@@ -87,14 +108,20 @@ exports.commands = {
     },
 
     votes: function (target, room, user) {
-        if (!this.canBroadcast()) return;
+        if (!this.canBroadcast()) {
+            return;
+        }
         this.sendReply('NUMBER OF VOTES: ' + Object.keys(Poll[room.id].options).length);
     },
 
     pr: 'pollremind',
     pollremind: function (target, room, user) {
-        if (!Poll[room.id].question) return this.sendReply('There is no poll currently going on in this room.');
-        if (!this.canBroadcast()) return;
+        if (!Poll[room.id].question) {
+            return this.sendReply('There is no poll currently going on in this room.');
+        }
+        if (!this.canBroadcast()) {
+            return;
+        }
         this.sendReplyBox(Poll[room.id].display);
     },
 }
